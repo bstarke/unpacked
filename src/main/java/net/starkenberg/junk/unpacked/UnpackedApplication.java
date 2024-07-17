42b1c571c3696bfe5bc0fb3c8b1f3d5f46f61eeb
Brad Starkenberg <bstarkenberg@vmware.com>
2024-07-17 11:49:29 -0500

    buildpack working with dist zip

--- src/main/java/net/starkenberg/junk/unpacked/UnpackedApplication.java
+++ src/main/java/net/starkenberg/junk/unpacked/UnpackedApplication.java
@@ -1,5 +1,7 @@
 package net.starkenberg.junk.unpacked;
 
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import java.util.Arrays;
 public class UnpackedApplication {
 
     public static void main(String[] args) {
+        Security.insertProviderAt(new BouncyCastleFipsProvider(), 0);
+        Security.insertProviderAt(new BouncyCastleJsseProvider(), 1);
         Arrays.stream(Security.getProviders()).map(Provider::getName).forEach(System.out::println);
         SpringApplication.run(UnpackedApplication.class, args);
     }
